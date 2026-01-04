6c1ec59ff6fdbd4c444a59c3b296c6adda817c8e
Vinicius Souza <contato@viniccius.com.br>
2026-01-04 17:00:17 -0300

    Initial Commit

---
+++ web/vite.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    TanStackRouterVite({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+});
